478eb98e946044a4795ec80a104ed6f5a331a87d
indietechjuliet <rachel.offutt2015@gmail.com>
2020-11-04 00:37:09 -0500

    added newst version of code

--- src/kpartiteclique.cpp
+++ src/kpartiteclique.cpp
@@ -1,72 +1,298 @@
 #include <cstdlib>
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <sstream>
-#include <map>
 #include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-class Graph
-{
-	public: 
-		Graph(string);
-		void readInGraph();
-	private:
-		int nodes;
-		string infile;
-		int edges;
-		map <string, vector < pair< string, int > > > edgeMap;
 
-};
 
-int main(int argc, char** argv)
+
+int findIndex(string target, vector < string> &nodes);
+void printMatrix( vector < vector <int> > &matrix);
+vector <int> returnFirstClique(vector <vector <int> > matrix, vector <int> partiteSets);
+void addIntrapartiteEdges(vector <vector < int> > &matrix, vector <int> partiteSets);
+void enumerate(vector <vector <int > > matrix, vector <int> R, vector <int> P, vector <int> X, vector <int> partiteSets );
+int choosePivot(vector <int> P , vector <int> X, vector < vector <int >> matrix);
+vector <int> intersection( vector <int> setA, vector <int> matrixRow);
+int coverPartition(vector <int> R, vector <int> partiteSets);
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[])
 {
-	if(argc != 2)
+
+	int partites = stoi(argv[2]);
+
+	cout << "PARTITE:" <<partites<<endl;
+	int vertices = 0;
+	int edges = 0;
+
+	vector < vector <int>> matrix;
+	vector <int> partiteSets;
+
+	for (int i = 0; i < partites; i++)
 	{
-		cerr<<"Invalid number of command line arguments.\nUsage: ./readGraph <graphfile>\n"<<
-			"Quitting now\n";
-		return -1;
+		string l = argv[i+3];
+		stringstream ss(l);
+
+		int min, max;
+		char trash;
+
+		ss>>min>>trash>>max;
+
+		partiteSets.push_back(min);
+
 	}
-
-	Graph G(argv[1]);
-
-	G.readInGraph();
-
-	return 0;
-}
-
-Graph::Graph(string f)
-{
-	infile = f;
-	nodes = -1;
-	edges = -1;
-}
-
-
-
-void Graph::readInGraph()
-{
+	cout<<partiteSets.size()<<endl;
+	for(int i = 0; i < partiteSets.size(); i++)
+		cout<<partiteSets[i]<<" ";
+	cout<<endl;
 	ifstream fin;
-	fin.open(infile);
-	
-	string line;
+	fin.open(argv[1]);
 
-	while(getline(fin, line))
-	{
+	string line;
+	vector <int> P;
+	while(getline(fin, line)){
 		stringstream ss(line);
-		if(nodes != -1)
+		if(vertices == 0)
 		{
-		
+			ss>>vertices>>edges;
+			//nodes.resize(vertices, "NA");
+			matrix.resize(vertices);
+			P.resize(vertices);
+			for(int i = 0; i < vertices; i++)
+				matrix[i].resize(vertices, 0);
+			for(int i = 0; i < P.size(); i++)
+				P[i] = i;
 		}
 		else
 		{
-			ss>>nodes>>edges;
+			cout<<line<<endl;
+			int node1, node2;
+			ss>>node1>>node2;
+
+			/*
+			   int i1 = findIndex(node1, nodes);
+			   nodes[i1] = node1;
+			   int i2 = findIndex(node2, nodes);
+			   nodes[i2] = node2;
+			   */
+
+			matrix[node1][node2] = 1;
+			matrix[node2][node1] = 1;
+
 		}
-		cout<<line<<endl;
+
 	}
-	fin.close();
-	cout<<"Nodes: "<<nodes<<endl;
-	cout<<"Edges: "<<edges<<endl;
+
+	printMatrix(matrix);
+
+	vector <int> clique = {2, 5, 7};
+
+	addIntrapartiteEdges(matrix,  partiteSets);
+	cout<<endl<<endl;
+	printMatrix(matrix);
+	vector <int> R;
+	vector < int> X;
+	enumerate(matrix, R, P, X, partiteSets);
+
+}
+
+void enumerate(vector <vector <int > > matrix, vector <int> R, vector <int> P, vector <int> X, vector <int> partiteSets )
+{
+	cout<<"here"<<endl;
+	cout<<"P: "<<P.size()<<"   X:"<<X.size()<<endl;
+	if(P.size() == 0 && X.size() == 0)
+	{
+		if(coverPartition(R, partiteSets))
+			cout<<"Maximal Partite CLique: "<<endl;
+		cout<<"returning"<<endl;
+		return;
+	}
+	int pivot = choosePivot(P, X, matrix);
+	cout<<"Pivot is: "<<pivot<<endl;
+	cout<<"R is currently: ";
+	for(int g = 0; g < R.size(); g++)
+		cout<<R[g]<<"  ";
+
+	cout<<endl;
+	for(int v = 0; v < matrix[pivot].size(); v++)
+	{
+		vector <int>::iterator it;
+		it = find(P.begin(), P.end(), v);
+		if(it != P.end() && matrix[pivot][v]  == 0)
+		{
+			R.push_back(v);
+
+			vector <int> PnNv = intersection(P, matrix[v]);
+			vector <int> XnNv = intersection(X, matrix[v]);
+			enumerate(matrix, R, PnNv, XnNv, partiteSets);
+			P.erase(it);
+			X.push_back(v);
+		}
+	}
+
+}
+
+
+vector <int> intersection( vector <int> setA, vector <int> matrixRow)
+{
+	vector <int>::iterator it;
+
+	vector <int> intersect;
+	for(int i = 0; i < matrixRow.size(); i++)
+	{
+		it = find(setA.begin(), setA.end(), i);
+		if(it != setA.end() && matrixRow[i] ==1)
+		{
+			intersect.push_back(i);
+		}
+	}
+	return intersect;
+}
+int choosePivot(vector <int> P , vector <int> X, vector < vector <int >> matrix)
+{
+	vector <int> PuX;
+
+	PuX = P;
+
+	vector<int>::iterator it;
+	for(int i = 0; i < X.size(); i++)
+	{
+		it  = find(PuX.begin(), PuX.end(), X[i]);
+		if(it == PuX.end())
+			PuX.push_back(X[i]);
+	}
+
+	int maxcount = 0;
+	int pivot;
+	for(int u = 0; u < PuX.size(); u++)
+	{
+		vector <int> Nu = matrix[PuX[u]];
+		int count  = 0;
+		for(int n = 0; n < Nu.size(); n++)
+		{
+			it = find(P.begin(), P.end(), n);
+			if(Nu[n] == 1 && it != P.end())
+				count++;
+		}
+		if(count > maxcount)
+		{
+			maxcount = count;
+			pivot = PuX[u];
+		}
+
+	}
+
+	return pivot;
+
+}
+
+int coverPartition(vector <int> R, vector <int> partiteSets)
+{
+	cout<<"IN COVER"<<endl;
+	vector <int> found;
+	found.resize(partiteSets.size(), 0);
+	for(int i = 0; i < partiteSets.size(); i++)
+	{
+		if(i+1 < partiteSets.size())
+		{
+			for(int node = 0; node < R.size(); node++)
+			{
+				if (R[node] > partiteSets[i] && R[node] < partiteSets[i+1])
+				{
+					found[i] = 1;
+					break;
+				}
+
+			}
+
+		}
+		else
+		{
+			for(int node = 0; R.size(); node++)
+			{
+				if(R[node] > partiteSets[i])
+				{
+					found[i] = 1;
+					break;
+				}
+			}
+
+		}
+	}
+
+	for(int i = 0; i < found.size(); i++)
+	{
+		if(found[i] == 0)
+			return false;
+	}
+	return true;
+
+}
+
+
+void addIntrapartiteEdges(vector <vector < int> > &matrix, vector <int> partiteSets)
+{
+	for(int i = 0; i < partiteSets.size(); i++)
+	{
+		if(i+1 < partiteSets.size())
+		{
+			int cap = partiteSets[i+1];
+			for(int a  = partiteSets[i]; a < cap; a++)
+			{
+				for(int b = partiteSets[i]; b < cap; b++)
+				{
+					matrix[a][b] = 1;
+					matrix[b][a] = 1;
+				}
+			}	
+
+		}
+		else
+		{
+			for(int a = partiteSets[i]; a < matrix[0].size(); a++)
+			{
+				for(int b = partiteSets[i]; b < matrix[0].size(); b++)
+				{
+					matrix[a][b] = 1;
+					matrix[b][a] =1;
+				}
+			}
+
+		}
+	}
+
+}
+
+
+
+void printMatrix( vector < vector <int> > &matrix)
+{
+	cout<<"	";
+	for(int i = 0; i < matrix[0].size(); i++)
+	{
+		cout<<i<<"	";
+	}
+	cout<<endl;
+	for(int i = 0; i < matrix[0].size(); i ++)
+	{
+		cout<<i;
+		for(int j = 0; j < matrix[0].size(); j++)
+		{
+			cout<<"	"<<matrix[i][j];
+		}
+		cout<<endl;
+	}
 }
 
